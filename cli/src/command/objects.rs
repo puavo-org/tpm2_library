017d77bddafc92866946b5b3f6be691a1f45147c
Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>
2025-08-14 04:08:39 +0300

    refactor(cli): output JSON in `objects`
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>

--- cli/src/command/objects.rs
+++ cli/src/command/objects.rs
@@ -2,7 +2,7 @@
 // Copyright (c) 2025 Opinsys Oy
 
 use crate::{cli, cli::Objects, AuthSession, Command, TpmDevice, TpmError};
-use tpm2_protocol::data::TpmRh;
+use tpm2_protocol::{data::TpmRh, TpmPersistent, TpmTransient};
 
 impl Command for Objects {
     /// Runs `objects`.
@@ -13,12 +13,16 @@ impl Command for Objects {
     fn run(&self, device: &mut TpmDevice, _session: Option<&AuthSession>) -> Result<(), TpmError> {
         let transient_handles = cli::get_handles(device, TpmRh::TransientFirst)?;
         for handle in transient_handles {
-            println!("transient:{handle:#010x}");
+            let obj = cli::Object::Handle(TpmTransient(handle));
+            let json_line = serde_json::to_string(&obj)?;
+            println!("{json_line}");
         }
 
         let persistent_handles = cli::get_handles(device, TpmRh::PersistentFirst)?;
         for handle in persistent_handles {
-            println!("persistent:{handle:#010x}");
+            let obj = cli::Object::Persistent(TpmPersistent(handle));
+            let json_line = serde_json::to_string(&obj)?;
+            println!("{json_line}");
         }
 
         Ok(())
