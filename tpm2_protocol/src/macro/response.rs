6296f2363d7633a91b5c1f6d995b1791b3bfb20c
Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>
2025-08-16 21:22:06 +0300

    refactor(tpm2_protocol): split macros to three files
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@opinsys.com>

---
+++ tpm2_protocol/src/macro/response.rs
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// Copyright (c) 2025 Opinsys Oy
+// Copyright (c) 2024-2025 Jarkko Sakkinen
+
+#[macro_export]
+macro_rules! tpm_response {
+    (
+        $(#[$meta:meta])*
+        $name:ident,
+        $cc:expr,
+        $no_sessions:expr,
+        $with_sessions:expr,
+        $(pub $handle_field:ident: $handle_type:ty,)*
+        {
+            $(pub $param_field:ident: $param_type:ty),*
+            $(,)?
+        }
+    ) => {
+        $(#[$meta])*
+        pub struct $name {
+            $(pub $handle_field: $handle_type,)*
+            $(pub $param_field: $param_type,)*
+        }
+
+        impl $crate::message::TpmHeader<'_> for $name {
+            const COMMAND: $crate::data::TpmCc = $cc;
+            const NO_SESSIONS: bool = $no_sessions;
+            const WITH_SESSIONS: bool = $with_sessions;
+            const HANDLES: usize = 0 $(+ {let _ = stringify!($handle_field); 1})*;
+        }
+
+        impl $crate::TpmSized for $name {
+            const SIZE: usize = 0 $(+ <$handle_type>::SIZE)* $(+ <$param_type>::SIZE)*;
+            fn len(&self) -> usize {
+                let params_len: usize = 0 $(+ self.$param_field.len())*;
+                let handles_len: usize = 0 $(+ self.$handle_field.len())*;
+                let parameter_area_size_field_len: usize = core::mem::size_of::<u32>();
+                handles_len + parameter_area_size_field_len + params_len
+            }
+        }
+
+        impl $crate::TpmBuild for $name {
+            fn build(&self, writer: &mut $crate::TpmWriter) -> $crate::TpmResult<()> {
+                let params_len: usize = 0 $(+ self.$param_field.len())*;
+                let params_len_u32 = u32::try_from(params_len)
+                    .map_err(|_| $crate::TpmErrorKind::ValueTooLarge)?;
+
+                $(self.$handle_field.build(writer)?;)*
+                params_len_u32.build(writer)?;
+                $(self.$param_field.build(writer)?;)*
+
+                Ok(())
+            }
+        }
+
+        impl<'a> $crate::TpmParse<'a> for $name {
+            fn parse(buf: &'a [u8]) -> $crate::TpmResult<(Self, &'a [u8])> {
+                #[allow(unused_mut)]
+                let mut cursor = buf;
+                $(
+                    let ($handle_field, tail) = <$handle_type>::parse(cursor)?;
+                    cursor = tail;
+                )*
+
+                #[allow(unused_mut)]
+                let (mut params, tail) = $crate::TpmParameters::new(cursor)?;
+                $(
+                    let $param_field = params.parse::<$param_type>()?;
+                )*
+                if !params.is_empty() {
+                    return Err($crate::TpmErrorKind::TrailingData);
+                }
+
+                Ok((
+                    Self {
+                        $( $handle_field, )*
+                        $( $param_field, )*
+                    },
+                    tail,
+                ))
+            }
+        }
+    };
+}
---
+++ tpm2_protocol/src/macro/struct.rs
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// Copyright (c) 2025 Opinsys Oy
+// Copyright (c) 2024-2025 Jarkko Sakkinen
+
+#[macro_export]
+macro_rules! tpm_struct {
+    (
+        $(#[$meta:meta])*
+        $name:ident,
+        $cc:expr,
+        $no_sessions:expr,
+        $with_sessions:expr,
+        $handles:expr,
+        {
+            $(pub $field_name:ident: $field_type:ty),*
+            $(,)?
+        }
+    ) => {
+        $crate::tpm_struct! {
+            $(#[$meta])*
+            pub struct $name {
+                $(pub $field_name: $field_type,)*
+            }
+        }
+
+        impl $crate::message::TpmHeader<'_> for $name {
+            const COMMAND: $crate::data::TpmCc = $cc;
+            const NO_SESSIONS: bool = $no_sessions;
+            const WITH_SESSIONS: bool = $with_sessions;
+            const HANDLES: usize = $handles;
+        }
+    };
+
+    (
+        $(#[$meta:meta])*
+        $vis:vis struct $name:ident {
+            $(pub $field_name:ident: $field_type:ty),*
+            $(,)?
+        }
+    ) => {
+        $(#[$meta])*
+        $vis struct $name {
+            $(pub $field_name: $field_type,)*
+        }
+
+        impl $crate::TpmSized for $name {
+            const SIZE: usize = 0 $(+ <$field_type>::SIZE)*;
+            fn len(&self) -> usize {
+                0 $(+ self.$field_name.len())*
+            }
+        }
+
+        impl $crate::TpmBuild for $name {
+            #[allow(unused_variables)]
+            fn build(&self, writer: &mut $crate::TpmWriter) -> $crate::TpmResult<()> {
+                $(self.$field_name.build(writer)?;)*
+                Ok(())
+            }
+        }
+
+        impl<'a> $crate::TpmParse<'a> for $name {
+            fn parse(buf: &'a [u8]) -> $crate::TpmResult<(Self, &'a [u8])> {
+                $(let ($field_name, buf) = <$field_type>::parse(buf)?;)*
+                Ok((
+                    Self {
+                        $($field_name,)*
+                    },
+                    buf,
+                ))
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! tpm_tagged_struct {
+    (
+        $(#[$outer:meta])*
+        $vis:vis struct $name:ident {
+            pub $tag_field:ident: $tag_ty:ty,
+            pub $value_field:ident: $value_ty:ty,
+        }
+    ) => {
+        $(#[$outer])*
+        $vis struct $name {
+            pub $tag_field: $tag_ty,
+            pub $value_field: $value_ty,
+        }
+
+        impl $crate::TpmTagged for $name {
+            type Tag = $tag_ty;
+            type Value = $value_ty;
+        }
+
+        impl $crate::TpmSized for $name {
+            const SIZE: usize = <$tag_ty>::SIZE + <$value_ty>::SIZE;
+            fn len(&self) -> usize {
+                self.$tag_field.len() + self.$value_field.len()
+            }
+        }
+
+        impl $crate::TpmBuild for $name {
+            fn build(&self, writer: &mut $crate::TpmWriter) -> $crate::TpmResult<()> {
+                self.$tag_field.build(writer)?;
+                self.$value_field.build(writer)
+            }
+        }
+
+        impl<'a> $crate::TpmParse<'a> for $name {
+            fn parse(buf: &'a [u8]) -> $crate::TpmResult<(Self, &'a [u8])> {
+                let ($tag_field, buf) = <$tag_ty>::parse(buf)?;
+                let ($value_field, buf) =
+                    <$value_ty as $crate::TpmParseTagged>::parse_tagged($tag_field, buf)?;
+                Ok((
+                    Self {
+                        $tag_field,
+                        $value_field,
+                    },
+                    buf,
+                ))
+            }
+        }
+    };
+}
